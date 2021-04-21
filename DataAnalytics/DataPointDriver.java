398cdc4b36b369ddc5c124e783c8cf6b2135344b
Ryan Warrick <ryanwarrick@carmis-air.attlocal.net>
2021-04-20 22:10:59 -0700

    Modified DataPointDriver.java to match Project Part 2's Instructions

--- DataAnalytics/DataPointDriver.java
+++ DataAnalytics/DataPointDriver.java
@@ -8,6 +8,13 @@ import java.util.ArrayList;
 public class DataPointDriver {
 
 	public static void main(String[] args) {
+		
+		// Project Part 2
+		
+		KNNPredictor knnPredictor = new KNNPredictor(1);
+		
+		knnPredictor.readData("titanic.csv");
+
 		// TODO Auto-generated method stub
 		
 		// write a method to put testData into a test file
@@ -16,37 +23,37 @@ public class DataPointDriver {
 		// ex: DummyPredictor D1 = new DummyPredictor();
 		// use D1.readData(filename)
 
-		DummyPredictor D1 = new DummyPredictor();
-		
-		ArrayList<DataPoint> newData1 = new ArrayList<DataPoint>();
-		newData1 = D1.readData("TestData.txt");
-		
-		Double newData2;
-		newData2 = D1.getAccuracy(newData1);
-		
-		String newData3;
-		newData3 = D1.getPrecision(newData1);
-		
-		// Display Data on a JFrame
-		JFrame frame = new JFrame();
-	
-		JButton showNewData1 = new JButton();
-		JButton showNewData2 = new JButton();
-		JButton showNewData3 = new JButton(newData3);
-		
-		JPanel panel = new JPanel();
-		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-		panel.setLayout(new GridLayout(0, 1));
-		panel.add(showNewData1);
-		panel.add(showNewData2);
-		panel.add(showNewData3);
-		
-		
-		frame.add(panel, BorderLayout.CENTER);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Project 1 Test Data");
-		frame.pack();
-		frame.setVisible(true);
+//		DummyPredictor D1 = new DummyPredictor();
+//		
+//		ArrayList<DataPoint> newData1 = new ArrayList<DataPoint>();
+//		newData1 = D1.readData("TestData.txt");
+//		
+//		Double newData2;
+//		newData2 = D1.getAccuracy(newData1);
+//		
+//		String newData3;
+//		newData3 = D1.getPrecision(newData1);
+//		
+//		// Display Data on a JFrame
+//		JFrame frame = new JFrame();
+//	
+//		JButton showNewData1 = new JButton();
+//		JButton showNewData2 = new JButton();
+//		JButton showNewData3 = new JButton(newData3);
+//		
+//		JPanel panel = new JPanel();
+//		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+//		panel.setLayout(new GridLayout(0, 1));
+//		panel.add(showNewData1);
+//		panel.add(showNewData2);
+//		panel.add(showNewData3);
+//		
+//		
+//		frame.add(panel, BorderLayout.CENTER);
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		frame.setTitle("Project 1 Test Data");
+//		frame.pack();
+//		frame.setVisible(true);
 		
 //		Container contentPane = frame.getContentPane();
 //		

a653d938af662de76f16885fa47bdb6716c08218
Ryan Warrick <ryanwarrick@carmis-air.attlocal.net>
2021-05-19 18:52:56 -0700

    Modified DataPoint.java file.

--- DataAnalytics/DataPoint.java
+++ DataAnalytics/DataPoint.java
@@ -1,79 +1,60 @@
 package dataanalytics;
 
 public class DataPoint {
-	private Double f1;
-	private Double f2;
-	private String label;
-	Boolean isTest;
-	
-	public DataPoint(Double f1, Double f2, String label, Boolean isTest) {
-		this.f1 = f1;
-		this.f2 = f2;
-		setLabel(label);
-		setIsTest(isTest);
-	}
-	
-	public DataPoint(Double f1, String label) {
-		this.setF1(f1);
-		this.setLabel(label);;
-	}
-	
-//	public DataPoint(Double f1,)
-	
-	public DataPoint(Double f1) {
-		this.f1 = f1;
-		this.label = null;
-	}
-	
-	public Double getF1() {
-		return this.f1;
-	}
-	
-	public Double getF2() {
-		return this.f2;
-	}
-	
-	public String getLabel() {
-		return this.label;
-	}
-	
-	public Boolean getIsTest() {
-		return this.isTest;
+	public double f1;
+	public double f2;
+	public String label;
+	public boolean isTest;
+
+	public DataPoint(double f1Param, double f2Param, String labelParam, boolean isTestParam) {
+		this.f1 = f1Param;
+		this.f2 = f2Param;
+		this.label = labelParam;
+		this.isTest = isTestParam;
 	}
 
-	public void setF1(Double f1) {
-		// Feel free to add some restrictions
-		if (f1 < 0) {
-			return;
-		}
-		this.f1 = f1;
+	public DataPoint() {
+		this.f1 = 0.0;
+		this.f2 = 0.0;
+		this.label = "";
+		this.isTest = false;
 	}
-	
-	public void setF2(Double f2) {
-		if (f2 < 0) {
-			return;
-		}
-		this.f2 = f2;
+
+	public double getF1() {
+		return f1;
+	}
+
+	public double getF2() {
+		return f2;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public boolean getIsTest() {
+		return isTest;
+	}
+
+	public void setF1(double f1Param) {
+		f1 = f1Param;
+	}
+
+	public void setF2(double f2Param) {
+		f2 = f2Param;
+	}
+
+	public void setLabel(String labelParam) {
+		label = labelParam;
+	}
+
+	public void setIsTest(boolean isTestParam) {
+		isTest = isTestParam;
 	}
 	
 	@Override
 	public String toString() {
 		return this.f1 + " " + this.f2 + " " + this.label + " " + this.isTest;
 	}
-	
-	public void setLabel(String label) {
-		if (! (label.equals("1") || label.equals("0"))) {
-			return;
-		}
-		this.label = label;
-	}
-	
-	public void setIsTest(Boolean isTest) {
-		if (! (isTest.equals(true) || isTest.equals(false))) {
-			return;
-		}
-		this.isTest = isTest;
-		
-	}
-	
+
 }
